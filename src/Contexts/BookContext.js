3dde9bfb398127185e81c83b0c0194b56b70b533
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2019-11-04 21:38:06 +0330

    Added some feature to bookcontext

--- src/Contexts/BookContext.js
+++ src/Contexts/BookContext.js
@@ -4,14 +4,19 @@ export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
 
-    const [books , setBook] = useState([]);
+    const [books , setBook] = useState([{title : 'The man called ove' , author : 'Fredric Bakman' , id : 1 }]);
 
-    const addBook = title => {
-        return setBook([...books , {title , id : books.length + 1}]);
+    const addBook = (title , author) => {
+        return setBook([...books , {title , author , id : books.length + 1}]);
+    }
+
+    const removeBook = (id) => {
+        let newBookList = books.filter(book => book.id !== id);
+        return setBook(newBookList);
     }
 
     return (
-        <BookContext.Provider value={{books , addBook}}>
+        <BookContext.Provider value={{books , addBook , removeBook}}>
             {props.children}
         </BookContext.Provider>
     )
